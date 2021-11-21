9fd74ce3dbc60b0700ec56c0c16a50b587c50253
cesargpita <c.gonzapita@gmail.com>
2021-11-21 12:35:02 +0000

    Hydrate the state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
+import { createTiles } from "../../misc/utils";
 
 import './App.css';
 
-class App extends Component{
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numTiles: 36,
+      playing: false,
+      previousTileIndex: null,
+      tiles: [],
+      toBeCleared: null
+    };
+  }
+
+  startGame(numtiles) {
+    console.log('startGame')
+    this.setState((state) => ({
+      ...state,
+      playing: true,
+      tiles: createTiles(numtiles)
+    }));
+  }
 
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Turbo-Matcher
-      </header>
-        <OptionsPanel />
-        <Board />
-      }
-    </div>
-  );
-
+    return (
+      <div className="App">
+        <header className="App-header">
+          Turbo-Matcher
+        </header>
+        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={() => this.startGame()} />
+        <Board tiles={this.state.tiles} />
+      </div>
+    );
   }
 }
 
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Board.css';
+import { Tile } from "../Tile";
 
-const Board = (props) => {
+const Board = ({ tiles, numTiles }) => {
 
+  const renderTiles = tiles.map(tile => <Tile {...tile} />)
   const gridConfig = {
-    gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
-    gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
+    gridTemplateColumns: `repeat(${Math.sqrt(numTiles)}, 1fr)`,
+    gridTemplateRows: `repeat(${Math.sqrt(numTiles)}, 1fr)`,
   }
 
 
   return (
     <div className='Board' style={gridConfig}>
+      {renderTiles}
     </div>
   )
 }
