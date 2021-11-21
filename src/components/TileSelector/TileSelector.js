24f67a0c81b8544b4a88534e25c11c99a4a98f75
cesargpita <c.gonzapita@gmail.com>
2021-11-21 14:01:22 +0000

    Corrections pt2

--- src/components/TileSelector/TileSelector.js
+++ src/components/TileSelector/TileSelector.js
@@ -8,16 +8,16 @@ const TileSelector = (props) => {
   const [ref, hovered] = useHover();
   const dropdown = hovered ? (
     <div className='tileSelectorContent' >
-      <div className='number' onClick={props.handleNumTileChange(4)}>4</div>
-      <div className='number' onClick={props.handleNumTileChange(16)}>16</div>
-      <div className='number' onClick={props.handleNumTileChange(36)}>36</div>
+      <div className='number' onClick={() => props.handleNumTileChange(4)}>4</div>
+      <div className='number' onClick={() => props.handleNumTileChange(16)}>16</div>
+      <div className='number' onClick={() => props.handleNumTileChange(36)}>36</div>
     </div>
   ) : null;
 
   return (
-    <div className='tileSelector' ref={ref}>
+    <div className='tileSelector'>
       <div>Number of Tiles</div>
-      <div className='tileSelectorDropdown'>
+      <div className='tileSelectorDropdown' ref={ref}>
         {props.numTiles}
         {dropdown}
       </div>
